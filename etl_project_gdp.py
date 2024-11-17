8fadd1954c356e74369e7beae148d3f1dae3f517
Imad NIDKOUCHI <168095047+Imadndk@users.noreply.github.com>
2024-11-17 12:38:46 +0100

    Update etl_project_gdp.py

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -4,60 +4,6 @@ import pandas as pd
 import numpy as np
 import sqlite3
 from datetime import datetime 
-url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
-table_attribs = ["Country", "GDP_USD_millions"]
-db_name = 'World_Economies.db'
-table_name = 'Countries_by_GDP'
-csv_path = './Countries_by_GDP.csv'
-def extract(url, table_attribs):
-    page = requests.get(url).text
-    data = BeautifulSoup(page,'html.parser')
-    df = pd.DataFrame(columns=table_attribs)
-    tables = data.find_all('tbody')
-    rows = tables[2].find_all('tr')
-    for row in rows:
-        col = row.find_all('td')
-        if len(col)!=0:
-            if col[0].find('a') is not None and '—' not in col[2]:
-                data_dict = {"Country": col[0].a.contents[0],
-                             "GDP_USD_millions": col[2].contents[0]}
-                df1 = pd.DataFrame(data_dict, index=[0])
-                df = pd.concat([df,df1], ignore_index=True)
-    return df
-
-def transform(df):
-    GDP_list = df["GDP_USD_millions"].tolist()
-    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
-    GDP_list = [np.round(x/1000,2) for x in GDP_list]
-    df["GDP_USD_millions"] = GDP_list
-    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
-    return df
-
-def load_to_csv(df, csv_path):
-    df.to_csv(csv_path)
-
-def load_to_db(df, sql_connect, table_name):
-    df.to_sql(table_name, sql_connect, if_exists='replace', index= False)
-
-def query_table(query_statement):
-    print(query_statement)
-    q=pd.read_sql(query_statement, sql_connect)
-    print(q)
-
-def logging(message):
-    time_format= "%Y-%h-%d-%H:%M:%S"
-    now=datetime.now()
-    timestamp=now.strftime(time_format)
-    with open(./etl_project_log.txt, 'a') as f:
-        f.write(timestamp+ ':' + message + '\n')
-
-
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-import numpy as np
-import sqlite3
-from datetime import datetime 
 url = 'https://web.archive.org/web/20230908091635 /https://en.wikipedia.org/wiki/List_of_largest_banks'
 table_attribs = ["Name", "MC_USD_Billion"]
 db_name = 'Banks.db'
